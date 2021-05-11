d0f04675470b4fee0b3ecdb326a979ae42ff726e
codicate <codicate@gmail.com>
2021-05-11 00:25:11 -0400

    lobby

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,13 +1,21 @@
+import styles from 'pages/Lobby.module.scss';
 
 import { useAppSelector } from 'app/hooks';
 import { selectRoomInfo } from 'app/roomSlice';
 
 const Lobby = () => {
   const roomInfo = useAppSelector(selectRoomInfo);
-  console.log('roomInfo', roomInfo);
+
   return (
-    <div>
-      <p>{roomInfo.roomId}</p>
+    <div id={styles.lobby}>
+      <button
+        id={styles.roomId}
+        onClick={async () => {
+          await navigator.clipboard.writeText(roomInfo.roomId);
+        }}
+      >
+        {roomInfo.roomId}
+      </button>
     </div>
   );
 };
